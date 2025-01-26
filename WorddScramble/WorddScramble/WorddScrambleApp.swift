ad253bf88aecd02a76b8900c17c1601aadb44c75
XJ <xjtan99@gmail.com>
2025-01-26 21:16:32 +0800

    Project05 Initialise

---
+++ WorddScramble/WorddScramble/WorddScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorddScrambleApp.swift
+//  WorddScramble
+//
+//  Created by Tan Xin Jie on 26/1/25.
+//
+
+import SwiftUI
+
+@main
+struct WorddScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
