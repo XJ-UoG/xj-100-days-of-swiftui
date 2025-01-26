6eff593c2840d156c2625f38ac62a43f952fb228
XJ <xjtan99@gmail.com>
2025-01-26 22:01:59 +0800

    Added functionalities and features - Length Check, Possible Answers, Give Up and Reset

--- WorddScramble/WorddScramble/ContentView.swift
+++ WorddScramble/WorddScramble/ContentView.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var usedWords = [String]()
+    @State private var possibleWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
     
     @State private var errorTitle = ""
     @State private var errorMessage = ""
     @State private var showingError = false
+    
+    @State private var isGameOver = false
     
     var body: some View {
         NavigationStack {
@@ -24,11 +27,15 @@ struct ContentView: View {
                         .textInputAutocapitalization(.never)
                 }
                 
-                Section {
-                    ForEach(usedWords, id: \.self) { word in
+                Section(header: Text("Guess the words")) {
+                    ForEach(possibleWords, id: \.self) { word in
                         HStack {
                             Image(systemName: "\(word.count).circle")
-                            Text(word)
+                            if !isGameOver {
+                                Text(usedWords.contains(word) ? word : String(repeating: "*", count: word.count))
+                            } else {
+                                Text(word)
+                            }
                         }
                     }
                 }
@@ -39,6 +46,19 @@ struct ContentView: View {
             .alert(errorTitle, isPresented: $showingError) { } message: {
                 Text(errorMessage)
             }
+            .toolbar(content: {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        if isGameOver {
+                            startGame()
+                        }
+                        isGameOver.toggle()
+                    }) {
+                        Text(isGameOver ? "Reset" : "Give Up")
+                            .foregroundColor(isGameOver ? .secondary : .red)
+                    }
+                }
+            })
         }
     }
     
@@ -48,29 +68,39 @@ struct ContentView: View {
         guard answer.count > 0 else { return }
         
         guard isOriginal(word: answer) else {
-            wordError(title: "Word used", message: "Use a different word!")
+            wordError(title: "Word is used", message: "Use a different word!")
             return
         }
-        
         guard isPossible(word: answer) else {
-            wordError(title: "Word is not possible", message: "'\(rootWord)' not found in your word")
+            wordError(title: "Word is not possible", message: "'\(answer)' not found in your '\(rootWord)'")
             return
         }
-        
+        guard isShort(word: answer) else {
+            wordError(title: "Word is too short", message: "Minimum word length is 3.")
+            return
+        }
         guard isReal(word: answer) else {
-            wordError(title: "Word does not exists", message: "???")
+            wordError(title: "Word does not exists", message: "Try again!")
             return
         }
-
         
         withAnimation {
             usedWords.insert(answer, at: 0)
+            if usedWords.count == possibleWords.count {
+                wordError(title: "Congratulations", message: "You successfully guessed all the words. Press reset to play again")
+                isGameOver = true
+            }
         }
+        
         newWord = ""
     }
     
     func isOriginal(word: String) -> Bool {
         !usedWords.contains(word)
+    }
+    
+    func isShort(word: String) -> Bool {
+        !(word.count < 3)
     }
     
     func isPossible(word: String) -> Bool {
@@ -104,10 +134,29 @@ struct ContentView: View {
             if let startWords = try? String(contentsOf: startWorldURL){
                 let allWords = startWords.components(separatedBy: "\n")
                 rootWord = allWords.randomElement() ?? "silkworm"
+                usedWords = []
+                findPossibleWords()
                 return
             }
         }
         fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func findPossibleWords() {
+        possibleWords = []
+        let rootLetters = Array(rootWord)
+        let wordLength = rootLetters.count
+        
+        for start in 0..<wordLength {
+            for end in start+1...wordLength {
+                let substring = String(rootLetters[start..<end])
+                
+                if substring != rootWord && substring.count >= 3 && isReal(word: substring) && !possibleWords.contains(substring) {
+                    possibleWords.append(substring)
+                }
+            }
+        }
+        possibleWords.sort(by: { $0.count < $1.count })
     }
 }
 
