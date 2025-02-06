7c32ccd4060f2b5e186b6eba86b766dcacd7475a
Tan Xin Jie <xjtan99@gmail.com>
2025-02-06 23:41:45 +0800

    basic ui app flow

--- Project13/Instafilter/Instafilter/ContentView.swift
+++ Project13/Instafilter/Instafilter/ContentView.swift
@@ -8,35 +8,77 @@
 import SwiftUI
 import CoreImage
 import CoreImage.CIFilterBuiltins
+import PhotosUI
 
 struct ContentView: View {
-    @State private var image: Image?
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    
+    @State private var selectedItem: PhotosPickerItem?
+    
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
     
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Import a photo to get started"))
+                    }
+                }
+                .onChange(of: selectedItem, loadImage)
+
+                Spacer()
+
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, applyProcessing)
+                }
+                .padding(.vertical)
+
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                    Spacer()
+
+                    // share the picture
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
         }
-        // .onAppear(perform: loadImage)
     }
     
-//    func loadImage() {
-//        let inputImage = UIImage(resource: .example)
-//        let beginImage = CIImage(image: inputImage)
-//
-//        let context = CIContext()
-//        let currentFilter = CIFilter.sepiaTone()
-//        
-//        currentFilter.inputImage = beginImage
-//        currentFilter.intensity = 1
-//        
-//        guard let outputImage = currentFilter.outputImage else { return }
-//        guard let cgImage =  context.createCGImage(outputImage, from: outputImage.extent) else { return }
-//        let uiImage = UIImage(cgImage: cgImage)
-//        
-//        image = Image(uiImage: uiImage)
-//    }
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
+        }
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+
+        let uiImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uiImage)
+    }
+    
+    func changeFilter() {
+    }
 }
 
 #Preview {
